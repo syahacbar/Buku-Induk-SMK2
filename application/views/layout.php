741b59ce24eebf3c343e0c6171ac166939d6d803
syahacbar <syahacbar@gmail.com>
2021-10-31 19:58:48 +0700

    Hidup Bartho !!!

--- application/views/layout.php
+++ application/views/layout.php
@@ -65,7 +65,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="index.html">SB Admin v2.0</a>
+                <a class="navbar-brand" href="index.html">Buku Induk SMK2 Mkw</a>
             </div>
             <!-- /.navbar-header -->
 
@@ -282,17 +282,6 @@
             <div class="navbar-default sidebar" role="navigation">
                 <div class="sidebar-nav navbar-collapse">
                     <ul class="nav" id="side-menu">
-                        <li class="sidebar-search">
-                            <div class="input-group custom-search-form">
-                                <input type="text" class="form-control" placeholder="Search...">
-                                <span class="input-group-btn">
-                                <button class="btn btn-default" type="button">
-                                    <i class="fa fa-search"></i>
-                                </button>
-                            </span>
-                            </div>
-                            <!-- /input-group -->
-                        </li>
                         <li>
                             <a href="<?php echo site_url('welcome');?>"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                         </li>
